1d47441c1b5269f0a4fa45568062622142970e80
Jose <jrobles@ip-172-31-48-229.ec2.internal>
2016-08-28 05:09:21 +0000

    User benchmarks and test cleanup

--- src/user_test.go
+++ src/user_test.go
@@ -4,19 +4,42 @@ import (
 	"testing"
 )
 
-func TestSwap(t *testing.T) {
-	early := Reviewer{Github: "EarlyCuyler", Flowdock: "bootyHunter"}
-	rusty := Reviewer{Github: "RustyCuyler", Flowdock: "rustyRules"}
-	durwood := Reviewer{Github: "DurwoodCuyler", Flowdock: "notASquid"}
+var (
+	early   = Reviewer{Github: "EarlyCuyler", Flowdock: "bootyHunter"}
+	rusty   = Reviewer{Github: "RustyCuyler", Flowdock: "rustyRules"}
+	durwood = Reviewer{Github: "DurwoodCuyler", Flowdock: "notASquid"}
+	lil     = Reviewer{Github: "LilCuyler", Flowdock: "tooDrunk2Care"}
+	dan     = Reviewer{Github: "DanHalen", Flowdock: "theWorldIsMine"}
 
-	reviewers := []Reviewer{
+	prAuthor = early.Github
+
+	reviewers = []Reviewer{
 		early,
 		rusty,
 		durwood,
+		lil,
+		dan,
 	}
 
-	swap(reviewers, 1, 2)
+	users = Config{
+		Users_Git_Flow: reviewers,
+	}
+)
 
+func BenchmarkSwap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		swap(reviewers, 1, 2)
+	}
+}
+
+func BenchmarkSelectReviewers(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		selectReviewers(prAuthor, users)
+	}
+}
+
+func TestSwap(t *testing.T) {
+	swap(reviewers, 1, 2)
 	if reviewers[1] == early {
 		t.Fatal("ERROR: user.swap failed")
 	}
@@ -26,36 +49,17 @@ func TestSwap(t *testing.T) {
 }
 
 func TestSelectReviewers(t *testing.T) {
-	u0 := Reviewer{Github: "User0", Flowdock: "user test0"}
-	u1 := Reviewer{Github: "User1", Flowdock: "user test1"}
-	u2 := Reviewer{Github: "User2", Flowdock: "user test2"}
-	u3 := Reviewer{Github: "User3", Flowdock: "user test3"}
-	u4 := Reviewer{Github: "User4", Flowdock: "user test4"}
-	u5 := Reviewer{Github: "User5", Flowdock: "user test5"}
-
-	temp := []Reviewer{
-		u0,
-		u1,
-		u2,
-		u3,
-		u4,
-		u5,
-	}
-	users := Config{
-		Users_Git_Flow: temp,
-	}
-	prOwner := "User0"
-	rev1, rev2 := selectReviewers(prOwner, users)
-	if rev1.Flowdock == "" {
+	reviewerA, reviewerB := selectReviewers(prAuthor, users)
+	if reviewerA.Flowdock == "" {
 		t.Fatal("rev1 can not be empty")
 	}
-	if rev2.Github == "" {
+	if reviewerB.Github == "" {
 		t.Fatal("rev2 can not be empty")
 	}
-	if rev1 == rev2 {
+	if reviewerA == reviewerB {
 		t.Fatal("rev2 and rev1 can not be the same")
 	}
-	if rev1.Github == prOwner || rev2.Github == prOwner {
+	if reviewerA.Github == prAuthor || reviewerB.Github == prAuthor {
 		t.Fatal("revs can not be equal to the prOwner")
 	}
 }
